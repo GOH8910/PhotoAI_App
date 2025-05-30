d480bea3fd290d59261b72789faa18a4dd2fe924
GOH8910 <146818589+GOH8910@users.noreply.github.com>
2025-05-30 15:28:22 +0100

    Initial Commit

---
+++ PhotoAI/PhotoAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAIApp.swift
+//  PhotoAI
+//
+//  Created by Edward Goh on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoAITests/PhotoAITests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAITests.swift
+//  PhotoAITests
+//
+//  Created by Edward Goh on 30/05/2025.
+//
+
+import Testing
+@testable import PhotoAI
+
+struct PhotoAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
